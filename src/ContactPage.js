761c796cd269ee1ca0a18baa3fc13a9272632d02
kolimisneha <166527564+kolimisneha@users.noreply.github.com>
2024-06-20 15:03:56 +0530

    App.css updated

---
+++ src/ContactPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ContactPage() {
+  return (
+    <div className="container mt-5">
+      <h1>Contact Us</h1>
+      <h5>
+        name:Ecommercewebsite.com
+        </h5>
+        <h5>
+        mail:ecommerce@gmail.com
+        </h5><h5>
+        mobile:6301823562
+      </h5>
+
+    </div>
+  );
+}
+
+export default ContactPage;
